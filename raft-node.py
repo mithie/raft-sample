dc90abd275d41bd073fde4a2d011f7fbd8e9512d
Q281756 <michael.thiem@bmw.de>
2025-01-13 11:45:38 +0100

    introduce heartbeats from leader to followers

--- raft-node.py
+++ raft-node.py
@@ -22,14 +22,19 @@ class RaftNode:
         self.current_term = 0
         self.voted_for_node = None
         self.leader_id = None
-        self.election_timeout = random.uniform(5, 10)
+        self.election_timeout_task = None
+        self.election_timeout_duration = random.uniform(5, 10)
         self.node_mapping = {i: node for i, node in enumerate(self.nodes)} 
 
-    async def start_election(self):
-        while True:
-            await asyncio.sleep(self.election_timeout)
-            if self.state == FOLLOWER:
-                await self.elect()
+    async def reset_election_timeout(self):
+        if self.election_timeout_task:
+            self.election_timeout_task.cancel()
+        self.election_timeout_task = asyncio.create_task(self.election_timer())
+
+    async def election_timer(self):
+        await asyncio.sleep(self.election_timeout_duration)
+        if self.state == FOLLOWER:
+            await self.elect()
 
     async def elect(self):
         try:
@@ -63,11 +68,56 @@ class RaftNode:
                 self.state = LEADER
                 self.leader_id = self.node_id
                 logger.debug(f"Node {self.node_mapping[self.node_id]}: Became leader for term {self.current_term}")
+
+                await self.send_heartbeats()
             else:
                 self.state = FOLLOWER
-                logger.debug(f"{self.node_id}: Election lost, reverting to follower")
+                logger.debug(f"{self.node_mapping[self.node_id]}: Election lost, reverting to follower")
         except Exception as e:
-            logger.debug(f"Error during election: {e}")                
+            logger.debug(f"Error during election: {e}")
+
+    async def send_heartbeats(self):
+        while self.state == LEADER:
+            append_entries = [self.send_append_entries(node) for id, node in self.node_mapping.items() if id != self.node_id]
+            await asyncio.gather(*append_entries)
+            await asyncio.sleep(1)
+    
+    async def send_append_entries(self, node):
+        logger.debug(f"Sending empty append entry to Node: {node}")
+
+        try:
+
+            async with aiohttp.ClientSession() as session:
+                async with session.post(f'http://{node}/append_entries', json={
+                    "term": self.current_term,
+                    "leader_id": self.node_id,
+                    "entries": []
+                }) as response:
+                    logger.debug(f"Send Append Entries request to {node} responded with {response.status}")
+                    if response.status == 200:
+                        data = await response.json()
+                        if data["term"] > self.current_term:
+                            self.state = FOLLOWER
+                            self.current_term = data["term"]
+        except Exception as e:
+            logger.debug(f"Error while sending empty append entries (heartbeats): {e}")
+
+
+    async def handle_append_entries(self, request):
+        data = await request.json()
+        term = data['term']
+        leader_id = data["leader_id"]
+        
+        if self.current_term > term:
+            return web.json_response({"term": self.current_term, "success": False})
+
+        self.current_term = term
+        self.state = FOLLOWER
+        self.leader_id = leader_id
+
+        await self.reset_election_timeout()
+
+        return web.json_response({"term": self.current_term, "success": True})
 
     async def handle_vote_request(self, request):
         data = await request.json()
@@ -92,7 +142,7 @@ class RaftNode:
         })
 
     async def run(self):
-        election_task = asyncio.create_task(self.start_election())
+        await self.reset_election_timeout()
         await self.run_server()
         await election_task
 
@@ -100,6 +150,7 @@ class RaftNode:
         try:
             app = web.Application()
             app.router.add_post('/vote', self.handle_vote_request)
+            app.router.add_post('/send_append_entries', self.handle_append_entries)
             runner = web.AppRunner(app)
             await runner.setup()
             site = web.TCPSite(runner, '0.0.0.0', 8000 + self.node_id)
